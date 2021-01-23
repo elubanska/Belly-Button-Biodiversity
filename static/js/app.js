4b514a10ff17057f3ef4b0da364b2a0c34d6aaa0
Emilia <e.lubanska@gmail.com>
2021-01-22 20:55:18 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -11,8 +11,8 @@ d3.json("samples.json").then((data)=> {
         .property("value",sample);
     });
     var samplevalue = name[0];
-    metadata(samplevalue)
-    createChart(samplevalue)
+    metadata(samplevalue);
+    createChart(samplevalue);
 });
 }
 
@@ -22,14 +22,14 @@ function metadata(datasample){
     d3.json("samples.json").then((data)=> {
         //console.log(data)
         var metadata = data.metadata;
-      /*  console.log(metadata);
-        console.log(datasample);*/
+        console.log(metadata);
+        console.log(datasample);
         var dataarray = metadata.filter(sampleobject => sampleobject.id == datasample);
         var result = dataarray[0];
-        var desplay = d3.select("#sample-metadata");
-        desplay.html("")
+        var display = d3.select("#sample-metadata");
+        display.html("")
         Object.entries(result).forEach(([key,value])=> {
-            desplay.append("h6").text(`${key} ${value}`);
+            display.append("h6").text(`${key} ${value}`);
         })
     })
 }
@@ -41,23 +41,30 @@ function optionChanged(newsample){
 
 function createChart(sampleid){
     d3.json("samples.json").then((data)=> {
-        //console.log(data)
+        //Sample data read
         var sampledata = data.samples;
         var dataarray = sampledata.filter(sampleobject => sampleobject.id == sampleid);
         var result = dataarray[0];
-        console.log(result)
-        var outid = result.otu_ids;
+        var otuid = result.otu_ids;
         var lables = result.otu_lables;
         var samples = result.sample_values;
+        
+        //Metadata read
+        var metadata = data.metadata;
+        var mdarray = metadata.filter(samplemd => samplemd.id == sampleid);
+        var mdresult = mdarray[0];
+        var wfreq = mdresult.wfreq;
+        //console.log(wfreq);
 
+        //Create Bubble chart
         var bubbledata = [{
-            x: outid,
+            x: otuid,
             y: samples,
             text: lables,
             mode: "markers",
             marker: {
                 size: samples,
-                color: outid,
+                color: otuid,
                 colorscale: "Earth"
             }
         }];
@@ -66,6 +73,64 @@ function createChart(sampleid){
             xaxis: {title: 'OTU ID'},
             hovermode: "closest"
           };
+        //Plot Bubble chart
         Plotly.newPlot("bubble", bubbledata, bubbleLayout);
-});
+
+        //Create Bar chart
+        var ylabel = otuid.slice(0,10).reverse().map(otuid => "OTU " + otuid);
+        var bardata = [{
+            type: 'bar',
+            x: samples.slice(0,10).reverse(),
+            y: ylabel,
+            orientation: 'h',
+            hovertext: lables
+        }];
+        var barLayout = {
+            title: 'Top 10 microbial species found',
+            xaxis: {title: 'SAMPLE'}
+        };
+        //Plot Bar chart
+        Plotly.newPlot("bar", bardata, barLayout)
+
+        var gaugeData = [{
+                
+                value: wfreq,
+                title: { text: "Belly Button Washing Frequency"},
+                
+                subtitle: { text: "Scrubs per Week" },
+                type: "indicator",
+                mode: "gauge",
+                gauge: {
+                    axis: { range: [0, 9], borderwidth: 0 },
+                    bar: { color: "darkblue" },
+                    //bgcolor: "white",
+                    //borderwidth: 2,
+                    //bordercolor: "gray",
+                    steps: [
+                      { range: [0, 1], color: "cyan" },
+                      { range: [1, 2], color: "cyan" },
+                      { range: [2, 3], color: "cyan" },
+                      { range: [3, 4], color: "cyan" },
+                      { range: [4, 5], color: "cyan" },
+                      { range: [5, 6], color: "cyan" },
+                      { range: [6, 7], color: "cyan" },
+                      { range: [7, 8], color: "cyan" },
+                      { range: [8, 9], color: "royalblue" }
+                    ]
+                }
+            }
+          ];
+          
+          var gaugeLayout = {
+            width: 600,
+            height: 500
+            //margin: { t: 0, b: 0 },
+            //paper_bgcolor: "lavender",
+            //font: { color: "darkblue", family: "Arial" }
+          };
+          
+          Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
+
+    });
 }
